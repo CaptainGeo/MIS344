b1e102b6ee567eec876a8bbe860b7952ca640755
CaptianGeo <earthvexingtimelord@gmail.com>
2019-04-17 19:57:11 -0700

    Fixed Age in new report

---
+++ Lab1/Lab1_PartD2_AH/Lab1_PartD2_AH/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab1_PartD2_AH")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab1_PartD2_AH")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df1c5f90-1886-4456-a9be-b3b607806d29")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
