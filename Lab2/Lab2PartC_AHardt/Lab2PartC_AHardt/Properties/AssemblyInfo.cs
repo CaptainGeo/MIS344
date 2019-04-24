608adac4f9a498e28aa04113658c9401456169dd
CaptianGeo <earthvexingtimelord@gmail.com>
2019-04-24 08:46:49 -0700

    Finished Part 3

---
+++ Lab2/Lab2PartC_AHardt/Lab2PartC_AHardt/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab2PartC_AHardt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2PartC_AHardt")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb72cbec-b067-4f30-8957-bd378c8481e8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
